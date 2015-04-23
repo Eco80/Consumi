b15e356cb0e3bd73d10127583415980940c3d48d
Eco80 <strocchienr@gmail.com>
2015-04-23 22:48:34 +0200

    Implementata List View

--- src/org/folug/consumi/MainActivity.java
+++ src/org/folug/consumi/MainActivity.java
@@ -2,11 +2,13 @@ package org.folug.consumi;
 
 import java.util.GregorianCalendar;
 
+import org.folug.consumi.elencorifornimenti.RifornimentiRowAdapter;
 import org.folug.consumi.model.Carburante;
 import org.folug.consumi.model.Veicolo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +20,13 @@ public class MainActivity extends Activity {
 		Veicolo veicolo = new Veicolo("Punto 1.4", "Fiat", 60000, Carburante.BENZINA);
 		veicolo.addRifornimento(Carburante.BENZINA, 60500, 40.0, 60.0, new GregorianCalendar(2015, 03, 1, 8, 0));
 		veicolo.addRifornimento(Carburante.BENZINA, 61000, 40.0, 60.0, new GregorianCalendar(2015, 04, 1, 8, 0));
+		veicolo.addRifornimento(Carburante.METANO, 61500, 12.0, 11.5, new GregorianCalendar(2015, 04, 1, 9, 0));
+		
+		
+		ListView lv = (ListView)findViewById(R.id.RiflistView);
+	
+		RifornimentiRowAdapter adapter = new RifornimentiRowAdapter(this, R.layout.rifornimenti_row, veicolo.getRifornimenti());
+		lv.setAdapter(adapter);
 	}
 
 }
--- src/org/folug/consumi/elencorifornimenti/RifornimentiRowAdapter.java
+++ src/org/folug/consumi/elencorifornimenti/RifornimentiRowAdapter.java
@@ -1,9 +1,11 @@
-package org.folug.consumi;
+package org.folug.consumi.elencorifornimenti;
 
-
-
+import java.text.SimpleDateFormat;
 import java.util.List;
+
+import org.folug.consumi.R;
 import org.folug.consumi.model.Rifornimento;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,11 +14,12 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 public class RifornimentiRowAdapter extends ArrayAdapter<Rifornimento> {
-
+	
 	public RifornimentiRowAdapter(Context context, int resource, List<Rifornimento> objects) {
 		super(context, resource, objects);
 		// TODO Auto-generated constructor stub
 	}
+	
 
 	/* (non-Javadoc)
 	 * @see android.widget.ArrayAdapter#getView(int, android.view.View, android.view.ViewGroup)
@@ -26,11 +29,13 @@ public class RifornimentiRowAdapter extends ArrayAdapter<Rifornimento> {
 		// TODO Auto-generated method stub
 		LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		Rifornimento r = getItem(position);
-		convertView = inflater.inflate(android.R.layout.simple_list_item_2, null);
-		TextView uptext = (TextView)convertView.findViewById(android.R.id.text1);
-		uptext.setText(r.getCarburante().name());
-		TextView downtext = (TextView)convertView.findViewById(android.R.id.text2);
-		downtext.setText("data");
+		convertView = inflater.inflate(R.layout.rifornimenti_row, null);
+		TextView descrizione = (TextView)convertView.findViewById(R.id.tvTipoRifornimento);
+		descrizione.setText(r.getCarburante().name());
+		TextView data = (TextView)convertView.findViewById(R.id.tvData);
+		SimpleDateFormat format1 = new SimpleDateFormat("dd-MM-yyyy hh:mm");
+		String d = format1.format(r.getData().getTime());
+		data.setText(d);
 		return convertView;
 	}
 
